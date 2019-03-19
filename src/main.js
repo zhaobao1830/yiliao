bb74e10af7cd7493dab518be067ff9ba50014d71
zhaobao1830 <x5219438>
2019-03-19 17:56:34 +0800

    1111

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
+import '@babel/polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import 'assets/styles/index.scss'
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
