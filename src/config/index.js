6381e2d0c917ef3a91ca626130d078a8692bf7dd
zhaobao1830 <x5219438>
2019-03-19 17:53:41 +0800

    config配置文件，以后与api有关的，都在这里配置

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+const config = {
+  baseUrl: 'http://cms.lin.7yue.pro/',
+  stagnateTime: 1 * 60 * 60 * 1000, // 无操作停滞时间  默认1小时
+  openAutoJumpOut: false, // 是否开启无操作跳出
+  api: {
+    codeUrl: '' // 请求验证码的地址
+  }
+}
+
+export default config
